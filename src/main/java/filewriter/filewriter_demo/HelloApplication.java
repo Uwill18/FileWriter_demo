a84ace38e12865dc06374acb893034042bf52684
Hoshi Holiday <hoshiholiday@gmail.com>
2023-05-08 14:38:26 -0500

    FileWriter

--- src/main/java/filewriter/filewriter_demo/HelloApplication.java
+++ src/main/java/filewriter/filewriter_demo/HelloApplication.java
@@ -6,18 +6,56 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.io.*;
+import java.util.Scanner;
+
+//source: https://wgu.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=01cbcd0a-6a5b-4193-b914-ab490112a69b
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+//        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+//        stage.setTitle("Hello!");
+//        stage.setScene(scene);
+//        stage.show();
     }
 
-    public static void main(String[] args) {
-        launch();
+    public static void main(String[] args) throws IOException{
+        //Filename variable
+        String filename ="src/main/java/files/groceries2.txt", item;
+
+        //Create Scanner object
+        Scanner keyboard = new Scanner(System.in);
+
+        //Get items count
+
+        System.out.print("How many items do you have? ");
+        int numItems = keyboard.nextInt();
+
+        //clear keyboard buffer
+        keyboard.nextLine();
+
+
+        //Create FileWriter object
+        FileWriter fwriter = new FileWriter(filename, true);
+
+
+        //Create and Open file
+        PrintWriter outputFile = new PrintWriter(fwriter);
+
+        //Get items and write to file
+        for(int i=0; i < numItems;i++){
+            System.out.print("Enter items "+(i+1) + ": ");
+            item = keyboard.nextLine();
+            outputFile.println(item);
+
+        }
+
+//Close file
+        outputFile.close();
+        System.out.println("File written!");
+
+
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module filewriter.filewriter_demo {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+
+
     opens filewriter.filewriter_demo to javafx.fxml;
     exports filewriter.filewriter_demo;
 }
